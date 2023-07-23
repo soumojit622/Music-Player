6b689672f84331aa74b22e3179125a15cdf12acc
Soumojit <soumojitbanerjee22@gmail.com>
2023-07-24 00:31:11 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Kotha Koiyo Na",
+    artist: "Coke Studio Bangla",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dekha Ek Khwab Mashup",
+    artist: "Sush & Yohan",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Raghav - Desperado (feat. Tesher)",
+    artist: "Raghav x Tesher",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Apna Bana Le x Strawberries & Cigarettes",
+    artist: "Sush & Yohan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mi Amor ",
+    artist: "Sharn, 40k & The Paul",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Shiv Tandav (Rock Version) Aigiri Nandini",
+    artist: "Sachet Tandon & Parampara Tandon",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Tere Hawaale",
+    artist: " Arijit Singh | Shilpa Rao",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Mon Re Krishikaj Janona",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
